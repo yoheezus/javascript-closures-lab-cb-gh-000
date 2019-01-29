d83d2197010f154923fca3638ae26e98c193bbce
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-29 21:11:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function  bumpCounter() {
+    var bumps = 0
+
+    function addBump() {
+        bumps += 1
+    }
+
+    function getBumps() {
+        return bumps
+    }
+
+    return {
+        addBump,
+        getBumps
+    }
+
+}
+
+function createAnimal(animalType) {
+    return deadlyDevice => {
+        return {animalType,  deadlyDevice}
+    }
+}
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
